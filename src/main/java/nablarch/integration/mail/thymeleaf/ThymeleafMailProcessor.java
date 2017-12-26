8faf02144f2c64b6d2d4fb231b2fe9150758bc18
Taichi Uragami <backpaper0@gmail.com>
2017-12-26 09:21:01 +0900

    定型メール送信のThymeleafアダプタを作成 (#1)
    * 定型メール送信のThymeleafアダプタを作成
    
    * 分かりやすさのためコメントを追加

---
+++ src/main/java/nablarch/integration/mail/thymeleaf/ThymeleafMailProcessor.java
@@ -0,0 +1,85 @@
+package nablarch.integration.mail.thymeleaf;
+
+import java.util.Map;
+
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.context.IContext;
+import org.thymeleaf.exceptions.TemplateEngineException;
+
+import nablarch.common.mail.TemplateEngineMailProcessor;
+import nablarch.common.mail.TemplateEngineProcessedResult;
+import nablarch.common.mail.TemplateEngineProcessingException;
+
+/**
+ * Thymeleafを使用する{@link TemplateEngineMailProcessor}の実装クラス。
+ * 
+ * @author Taichi Uragami
+ *
+ */
+public class ThymeleafMailProcessor implements TemplateEngineMailProcessor {
+
+    /** Thymeleafのテンプレートエンジン */
+    private ITemplateEngine templateEngine;
+
+    /** 件名と本文を分けるデリミタ */
+    private String delimiter;
+
+    /**
+     * テンプレートIDと言語から取得されたテンプレートと変数をマージして、その結果を返す。
+     * 
+     * <p>
+     * テンプレートの検索、テンプレートと変数のマージは{@link ITemplateEngine#process(String, IContext)}が使われる。
+     * </p>
+     * 
+     * @see ITemplateEngine#process(String, IContext)
+     */
+    @Override
+    public TemplateEngineProcessedResult process(String templateId, String lang,
+            Map<String, Object> variables) {
+        try {
+            IContext context = createContext(variables);
+            String processed = templateEngine.process(templateId, context);
+            if (delimiter != null) {
+                return TemplateEngineProcessedResult.valueOf(processed, delimiter);
+            }
+            return TemplateEngineProcessedResult.valueOf(processed);
+        } catch (TemplateEngineException e) {
+            throw new TemplateEngineProcessingException(e);
+        }
+    }
+
+    /**
+     * {@link IContext}を作成する。
+     * 
+     * @param variables {@link #process(String, String, Map)}に渡された変数
+     * @return {@link IContext}のインスタンス
+     */
+    protected IContext createContext(Map<String, Object> variables) {
+        Context context = new Context();
+        context.setVariables(variables);
+        return context;
+    }
+
+    /**
+     * Thymeleafのエントリーポイントとなる{@link ITemplateEngine}を設定する。
+     * 
+     * @param templateEngine Thymeleafのテンプレートエンジン
+     */
+    public void setTemplateEngine(ITemplateEngine templateEngine) {
+        this.templateEngine = templateEngine;
+    }
+
+    /**
+     * 件名と本文を分けるデリミタを設定する。
+     * 
+     * <p>
+     * なにも設定されていなければ{@link TemplateEngineProcessedResult#DEFAULT_DELIMITER デフォルトのデリミタ}が使用される。
+     * </p>
+     * 
+     * @param delimiter 件名と本文を分けるデリミタ
+     */
+    public void setDelimiter(String delimiter) {
+        this.delimiter = delimiter;
+    }
+}
---
+++ src/test/java/nablarch/integration/mail/thymeleaf/ThymeleafMailProcessorContainerManagedTest.java
@@ -0,0 +1,47 @@
+package nablarch.integration.mail.thymeleaf;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import nablarch.common.mail.TemplateEngineProcessedResult;
+import nablarch.core.repository.SystemRepository;
+import nablarch.test.support.SystemRepositoryResource;
+
+/**
+ * {@link ThymeleafMailProcessor}をコンポーネント設定ファイルで構築する場合のテストクラス。
+ */
+public class ThymeleafMailProcessorContainerManagedTest {
+
+    @Rule
+    public SystemRepositoryResource systemRepositoryResource = new SystemRepositoryResource(
+            "nablarch/integration/mail/thymeleaf/ThymeleafMailProcessorContainerManagedTest.xml");
+
+    /**
+     * コンポーネント設定ファイルで構築するテスト。
+     */
+    @Test
+    public void testProcessConfiguredByXml() {
+
+        ThymeleafMailProcessor sut = SystemRepository.get("templateEngineMailProcessor");
+
+        //テンプレートエンジンの処理をして設定済みITemplateEngineを
+        //使用できていることを確認する。
+        Map<String, Object> variables = new HashMap<String, Object>();
+        variables.put("foo", "0");
+        variables.put("bar", false);
+        variables.put("bazs", Arrays.asList("1", "2", "3"));
+        TemplateEngineProcessedResult result = sut.process(
+                "nablarch/integration/mail/thymeleaf/testProcessConfiguredByXml.txt", null,
+                variables);
+
+        assertThat(result.getSubject(), is("あああ0"));
+        assertThat(result.getMailBody(), is("いいい\nえええ1\nえええ2\nえええ3\n"));
+    }
+}
