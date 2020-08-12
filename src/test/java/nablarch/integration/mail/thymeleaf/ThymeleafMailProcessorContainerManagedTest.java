ec6050d11f5b28a8df6f617d55b5653eceba919a
kiyotis <ito.kiyohito@tis.co.jp>
2020-08-12 16:47:41 +0900

    プレースホルダと変数のMapが変更不可な場合のテストに変更。

--- src/test/java/nablarch/integration/mail/thymeleaf/ThymeleafMailProcessorContainerManagedTest.java
+++ src/test/java/nablarch/integration/mail/thymeleaf/ThymeleafMailProcessorContainerManagedTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -39,9 +40,9 @@ public class ThymeleafMailProcessorContainerManagedTest {
         variables.put("bazs", Arrays.asList("1", "2", "3"));
         TemplateEngineProcessedResult result = sut.process(
                 "nablarch/integration/mail/thymeleaf/testProcessConfiguredByXml.txt", null,
-                variables);
+                Collections.unmodifiableMap(variables));
 
         assertThat(result.getSubject(), is("あああ0"));
-        assertThat(result.getMailBody(), is("いいい\nえええ1\nえええ2\nえええ3\n"));
+        assertThat(result.getMailBody(), is("いいい\r\nえええ1\r\nえええ2\r\nえええ3\r\n"));
     }
 }
